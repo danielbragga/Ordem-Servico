9d08d5b3af80640c49ab0cd8139d579ac96fc217
Daniel Braga <danielbraga2121@gmail.com>
2024-07-08 23:44:15 -0300

    Montagem do template

---
+++ application/views/home/index.php
@@ -0,0 +1,21 @@
+<?php $this->load->view('layout/sidebar'); ?>
+
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+	<!-- Main Content -->
+	<div id="content">
+		<?php $this->load->view('layout/navbar'); ?>
+
+
+		<!-- Begin Page Content -->
+		<div class="container-fluid">
+
+			<!-- Page Heading -->
+			<h1 class="h3 mb-4 text-gray-800">Home</h1>
+
+		</div>
+		<!-- /.container-fluid -->
+
+	</div>
+	<!-- End of Main Content -->
