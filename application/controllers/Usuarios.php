ab81a6ac1d37a322299002349e098c86de9dd649
Daniel Braga <danielbraga2121@gmail.com>
2024-07-20 23:54:45 -0300

    feat: dinamizando o carregamento das folhas e estilo

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -11,6 +11,12 @@ class Usuarios extends CI_Controller
 	public function index()
 	{
 		$data = array(
+			'styles' => array('vendor/datatables/dataTables.bootstrap4.min.css'),
+			'scripts' => array(
+				'vendor/datatables/jquery.dataTables.min.js',
+				'vendor/datatables/dataTables.bootstrap4.min.js',
+				'js/demo/datatables-demo.js'
+			),
 			'usuarios' => $this->ion_auth->users()->result(),
 
 		);
